36f68bda14ff257b21a651bc3bfd50bd2f029bd1
U-NSIDC\windnage <windnage@Nilak.ad.int.nsidc.org>
2011-04-25 14:15:47 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '58b9a56a3bf5a1b1b23e62e97a4c45f34048b12c25992329eb5880120f399f8db2dbb917f24f7eff8faa6eecec4ded27d222c2f8a7baeb58610ebf1745d08c21'
